d35c25de9e3ec0d14e87b6be2e15712e7ad0dffe
ece sen <eecesenn@gmail.com>
2023-11-25 23:23:36 +0100

    rooms finished

--- rooms.php
+++ rooms.php
@@ -18,7 +18,8 @@ session_start();
     
     <div class="bg-image" style="background-image: url('./room_imgs/bg_rooms.jpg'); background-position:center; background-size:cover; background-repeat:no-repeat; height:100%;">
         <div class="container">
-        <div class="row row-cols-1 row-cols-md-2 g-4 pt-4 pb-4">
+        <div class="row row-cols-1 row-cols-md-2 g-4 pt-4 pb-5">
+            <!-- Single Room Card -->
             <div class="col">
                 <div class="card">
                 <img src="./room_imgs/single/single_room.jpg" class="card-img-top" alt="Single Zimmer">
@@ -26,8 +27,28 @@ session_start();
                     <h5 class="card-title">Single Zimmer</h5>
                     <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium reiciendis impedit at quis vitae eveniet vero sint molestiae ullam, sequi, aspernatur porro odit ex voluptates iste, ipsum suscipit eligendi aliquid!</p>
                 </div>
+                <button class="btn btn-light" 
+                    <?php if (!(isset($_SESSION["user"]) && $_SESSION["user"] == "admin")) { ?>data-bs-toggle="modal" data-bs-target="#Modal" <?php } else { ?> onclick="location.href='./reservation_process.php'" <?php } ?>>
+                    Jetzt Buchen 
+                </button> 
                 </div>
             </div>
+            <!-- Modal -->
+            <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="ModalLabel">Login erforderlich</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Bitte <a href="./login_page.php">login</a> Sie sich ein, um fortzufahren. Falls Sie noch kein Konto haben, können Sie sich hier <a href="./register_page.php">registrieren</a>.
+                </div>
+                </div>
+            </div>
+            </div>
+
+            <!-- Double Room Card -->
             <div class="col">
                 <div class="card">
                 <img src="./room_imgs/double/double_bedroom.jpg" class="card-img-top" alt="Double Zimmer">
@@ -35,8 +56,28 @@ session_start();
                     <h5 class="card-title">Double Zimmer</h5>
                     <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium reiciendis impedit at quis vitae eveniet vero sint molestiae ullam, sequi, aspernatur porro odit ex voluptates iste, ipsum suscipit eligendi aliquid!</p>
                 </div>
+                <button class="btn btn-light" 
+                    <?php if (!(isset($_SESSION["user"]) && $_SESSION["user"] == "admin")) { ?>data-bs-toggle="modal" data-bs-target="#Modal" <?php } else { ?> onclick="location.href='./reservation_process.php'" <?php } ?>>
+                    Jetzt Buchen 
+                </button> 
                 </div>
             </div>
+            <!-- Modal -->
+            <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="ModalLabel">Login erforderlich</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Bitte <a href="./login_page.php">login</a> Sie sich ein, um fortzufahren. Falls Sie noch kein Konto haben, können Sie sich hier <a href="./register_page.php">registrieren</a>.
+                </div>
+                </div>
+            </div>
+            </div>
+
+            <!-- Suite Room Card -->
             <div class="col">
                 <div class="card">
                 <img src="./room_imgs/suite/suite_bedroom.jpg" class="card-img-top" alt="Suite">
@@ -44,16 +85,54 @@ session_start();
                     <h5 class="card-title">Suite Zimmer</h5>
                     <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium reiciendis impedit at quis vitae eveniet vero sint molestiae ullam, sequi, aspernatur porro odit ex voluptates iste, ipsum suscipit eligendi aliquid!</p>
                 </div>
+                <button class="btn btn-light" 
+                    <?php if (!(isset($_SESSION["user"]) && $_SESSION["user"] == "admin")) { ?>data-bs-toggle="modal" data-bs-target="#Modal" <?php } else { ?> onclick="location.href='./reservation_process.php'" <?php } ?>>
+                    Jetzt Buchen 
+                </button> 
                 </div>
             </div>
+            <!-- Modal -->
+            <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="ModalLabel">Login erforderlich</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Bitte <a href="./login_page.php">login</a> Sie sich ein, um fortzufahren. Falls Sie noch kein Konto haben, können Sie sich hier <a href="./register_page.php">registrieren</a>.
+                </div>
+                </div>
+            </div>
+            </div>
+            
+            <!-- Services Card -->
             <div class="col">
                 <div class="card">
-                <img src="./room_imgs/spa/spa_1.jpg" class="card-img-top" alt="...">
+                <img src="./room_imgs/spa/spa_1.jpg" class="card-img-top" alt="Spa">
                 <div class="card-body">
                     <h5 class="card-title">Spa & Massage Services</h5>
                     <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium reiciendis impedit at quis vitae eveniet vero sint molestiae ullam, sequi, aspernatur porro odit ex voluptates iste, ipsum suscipit eligendi aliquid!</p>
                 </div>
+                <button class="btn btn-light" 
+                    <?php if (!(isset($_SESSION["user"]) && $_SESSION["user"] == "admin")) { ?>data-bs-toggle="modal" data-bs-target="#Modal" <?php } else { ?> onclick="location.href='./reservation_process.php'" <?php } ?>>
+                    Jetzt Buchen 
+                </button> 
                 </div>
+                <!-- Modal -->
+                <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="ModalLabel">Login erforderlich</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Bitte <a href="./login_page.php">login</a> Sie sich ein, um fortzufahren. Falls Sie noch kein Konto haben, können Sie sich hier <a href="./register_page.php">registrieren</a>.
+                    </div>
+                    </div>
+                    </div>
+                    </div>
             </div>
             </div>
         </div>    
