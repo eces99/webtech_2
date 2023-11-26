4ab22aa91a9a01d759c82f00c5e3fcfa664e2b4d
ece sen <eecesenn@gmail.com>
2023-11-26 06:27:11 +0100

    reservations table needs repair :D

--- meine_reservations.php
+++ meine_reservations.php
@@ -1,40 +1,50 @@
 <?php
 session_start();
-
-if (isset($_SESSION['reservations']) && count($_SESSION['reservations']) > 0) {
-    echo "<h2>Your Reservations</h2>";
-    echo "<table border='1'>
-            <tr>
-                <th>Check-in</th>
-                <th>Check-out</th>
-                <th>Room Type</th>
-                <th>Services</th>
-            </tr>";
-
-    foreach ($_SESSION['reservations'] as $reservation) {
-        echo "<tr>
-                <td>" . $reservation['checkin'] . "</td>
-                <td>" . $reservation['checkout'] . "</td>
-                <td>" . $reservation['room_type'] . "</td>
-                <td>" . $reservation['services'] . "</td>
-            </tr>";
-    }
-
-    echo "</table>";
-} else {
-    echo "No reservations found.";
-}
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
     <title>Meine Reservierungen</title>
 </head>
-<body>
-    
+<body>   
+    <?php include("./navbar.php"); ?>
+    <div class="container">
+    <div class="display-4 text-bold row justify-content-center mt-4 mb-2">Your Reservations</div>
+        <div class="table-responsive">
+            <table class="table table-success table-striped">
+                    <?php if (isset($_SESSION['reservations']) && count($_SESSION['reservations']) > 0) { ?>
+                    <tr>
+                        <th>Check-in</th>
+                        <th>Check-out</th>
+                        <th>Room Type</th>
+                        <th>Services</th>
+                        <th>Services</th>
+                        <th>Services</th>
+                    </tr>
+                    <?php foreach ($_SESSION['reservations'] as $reservation) { ?>
+                    <tr>
+                        <td><?php echo $reservation['anreise'] ?></td>
+                        <td><?php echo $reservation['abreise'] ?></td>
+                        <td><?php echo $reservation['room'] ?></td>
+                        <td><?php echo $reservation['breakfast'] ?></td>
+                        <td><?php echo $reservation['park'] ?> </td>
+                        <td><?php echo $reservation['tiere'] ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    <?php } else { ?>
+        <p>No reservations found.</p>
+    <?php } ?>
+</div>
+
+<?php include './footer.php' ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
--- reservation_process.php
+++ reservation_process.php
@@ -2,35 +2,110 @@
 session_start();
 
 // Process form data
-$checkin = $_POST['checkin'];
-$checkout = $_POST['checkout'];
-$roomType = $_POST['room_type'];
-$services = isset($_POST['services']) ? implode(", ", $_POST['services']) : "";
+$conf_msg = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $anreise = $_POST['anreise'];
+    $abreise = $_POST['abreise'];
+    $room = $_POST['room'];
+    $breakfast = $_POST['breakfast'];
+    $park = $_POST['park'];
+    $tiere = $_POST['tiere'];
 
 // Store reservation data in the session
-$reservation = [
-    'checkin' => $checkin,
-    'checkout' => $checkout,
-    'room_type' => $roomType,
-    'services' => $services,
-];
+    $reservation = [
+        'anreise' => $anreise,
+        'abreise' => $abreise,
+        'room' => $room,
+        'breakfast'=> $breakfast,
+        'park'=> $park,
+        'tiere'=> $tiere
+    ];
 
-$_SESSION['reservations'][] = $reservation;
+    $i =0;
+    $_SESSION['reservations'][$i] = $reservation;
 
-echo "Reservation successful!";
+    $conf_msg = "Reservation successful! Sie können Ihre Reservierungen <a href='./meine_reservations.php'>hier</a> sehen.";
+    $i++;
+}
 
 // Optionally, you can redirect the user to a confirmation page
 // header("Location: confirmation.php");
 // exit();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <title>Reservierung</title>
 </head>
 <body>
-    
+<?php include "./navbar.php"; ?>
+<div class="bg-image" style="background-image: url('https://images.unsplash.com/photo-1503017964658-e2ff5a583c8e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-position:center; background-size:cover; background-repeat:no-repeat; height:100%;">
+
+<div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100" >
+      <div class="col col-xl-10">
+        <div class="" style="border-radius: 1rem;">
+          <div class="row g-0" >
+            <div class="col-md-6 col-lg-5 d-none d-md-block" >
+              <img src="./room_imgs/reg_process.jpg"
+                alt="res" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+            </div>
+            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="body p-4 p-lg-5 text-black">
+                <?php echo "<div class='display-8'> $conf_msg </div>" ?> 
+                <h1 class="h3 mb-3 font-weight-normal text-center">Anmeldeformular</h1>
+                <form action="" method="post">
+                    <div class="form-group mt-4">
+                        <label for="anreise">Anreisedatum</label>
+                        <input type="date" name ="anreise" class="form-control input-with-post-icon datepicker" inline="true" placeholder="Anreisedatum">
+                    </div>
+                    <div class="form-group mt-4">
+                        <label for="abreise">Abreisedatum</label>
+                        <input type="date" name ="abreise" class="form-control input-with-post-icon datepicker" inline="true" placeholder="Abreisedatum">
+                    </div>
+                    <select class="form-select mt-4" aria-label="room" name="room">
+                        <option selected disabled value="">Bitte wählen Sie den Zimmertyp...</option>
+                        <option value="Single">Single Zimmer</option>
+                        <option value="Double">Double Zimmer</option>
+                        <option value="Suite">Suite</option>
+                    </select>
+                    <select class="form-select mt-4" aria-label="breakfast" name="breakfast">
+                        <option selected disabled value="">Möchten Sie Frühstück?</option>
+                        <option value="Ja">Ja</option>
+                        <option value="Nein">Nein</option>
+                    </select>
+                    <select class="form-select mt-4" aria-label="park" name="park">
+                        <option selected disabled value="">Möchten Sie einen Parkplatz reservieren?</option>
+                        <option value="Ja">Ja</option>
+                        <option value="Nein">Nein</option>
+                    </select>
+                    <select class="form-select mt-4" aria-label="tiere" name="tiere">
+                        <option selected disabled value="">Bringen Sie Ihre Huastiere mit?</option>
+                        <option value="Ja">Ja</option>
+                        <option value="Nein">Nein</option>
+                    </select>
+                    <br>
+                    <div class="btns">
+                        <button type="submit" name="reservieren" value="reserieren" class="btn btn-dark">Reservieren</button>
+                    </div>
+                </form>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include './footer.php' ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
