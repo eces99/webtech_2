d0bba0e4b8e773214e90d8340a00914f7a7c5903
kenno10101 <kennsanga@gmail.com>
2023-11-26 17:33:47 +0100

    impressum + auto-redirect for non-users

--- impressum.php
+++ impressum.php
@@ -3,6 +3,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,18 +11,19 @@
     <link rel="stylesheet" href="./style.css">
     <title>Impressum</title>
 </head>
-<body>
-<?php include_once "./navbar.php" ?>
-<div class="container">
-    <div class="row justify-content-center">
-        
-    <div class="header-img" style="background-image: url('https://images.unsplash.com/photo-1517200578024-62d131797ec8?q=80&w=1414&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); height: 20vh; background-size: cover; background-position: center;">
 
-        <h1 class="display-2" style="color: white; text-align: center; padding: auto; font-weight:500;">Impressum</h1>
-    </div>
-        
-        <div class="col-md-4 text-center">
-        Informationspflicht laut §5 E-Commerce Gesetz, §14 Unternehmensgesetzbuch, §63 Gewerbeordnung und Offenlegungspflicht laut §25 Mediengesetz</br>
+<body>
+    <?php include_once "./navbar.php" ?>
+    <div class="container">
+        <div class="row justify-content-center">
+
+            <div class="header-img" style="background-image: url('https://images.unsplash.com/photo-1517200578024-62d131797ec8?q=80&w=1414&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); height: 20vh; background-size: cover; background-position: center;">
+
+                <h1 class="display-2" style="color: white; text-align: center; padding: auto; font-weight:500;">Impressum</h1>
+            </div>
+
+            <div class="col-md-4 text-center">
+                Informationspflicht laut §5 E-Commerce Gesetz, §14 Unternehmensgesetzbuch, §63 Gewerbeordnung und Offenlegungspflicht laut §25 Mediengesetz</br>
                 Hotel Moonlight GmbH</br>
                 Hotellerie</br>
                 UID-Nr: ATU12345678</br>
@@ -39,23 +41,30 @@
                 Bezirkshauptmannschaft Wien</br>
                 Meisterbetrieb</br>
                 Meisterprüfung abgelegt in Österreich
-                Verbraucher haben die Möglichkeit, Beschwerden an die OnlineStreitbeilegungsplattform 
+                Verbraucher haben die Möglichkeit, Beschwerden an die OnlineStreitbeilegungsplattform
                 der EU zu richten: <a href="http://ec.europa.eu/odr">ec.europa.eu/odr</a>. </br>
                 Sie können allfällige Beschwerde auch an die oben angegebene E-Mail-Adresse richten.</br>
-                Geschäftsführer: Ece Sen </br> 
-                Gesellschaftsanteile: Ece Sen </br>
+                Geschäftsführer: Ece Sen </br>
+                Stellvertretender Geschäftsführer: Kenn-Michael Sanga </br>
+                Gesellschaftsanteile: Ece Sen (50%), Kenn-Michael Sanga (50%) </br>
                 </br>
-        
-            <img src="./Images/AboutUsEce.png" class="image-fluid" alt="Ece Sen" style="max-width:70%;"> </br>
-            Ece Sen </br> 
-            Mobil: +43 6780 780 38 88</br>
-            E-mail: <a href="mailto:if23b062@technikum-wien.at">if23b062@technikum-wien.at</a></br>
-            <p style="margin-bottom: 5%;"></p>
+
+                <img src="./Images/AboutUsEce.png" class="image-fluid" alt="Ece Sen" style="max-width:70%;"> </br>
+                Ece Sen </br>
+                Mobil: +43 6780 780 38 88</br>
+                E-mail: <a href="mailto:if23b062@technikum-wien.at">if23b062@technikum-wien.at</a></br>
+                <p style="margin-bottom: 5%;"></p>
+                <img src="./Images/AboutUsKenn.png" class="image-fluid" alt="Kenn-Michael Sanga" style="max-width:70%;"> </br>
+                Kenn-Michael Sanga </br>
+                Mobil: +43 670 5085212</br>
+                E-mail: <a href="mailto:if23b128@technikum-wien.at">if23b128@technikum-wien.at</a></br>
+                <p style="margin-bottom: 5%;"></p>
+            </div>
         </div>
     </div>
-</div>
     <?php include_once './footer.php' ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
--- reservation_process.php
+++ reservation_process.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+if (!isset($_SESSION['user'])) {
+  header('Location: login_page.php');
+}
+
+
 // Process form data
 $conf_msg = $anreise = $abreise = $room = $park = $tiere = $breakfast = "";
 $error_msg = "Bitte füllen Sie sowohl das Anreisedatum als auch das Abreisedatum aus!";
@@ -15,42 +20,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
   if (isset($_POST['room'])) {
     $room = $_POST['room'];
-  } 
+  }
   if (isset($_POST['breakfast'])) {
     $breakfast = $_POST['breakfast'];
-  } 
+  }
   if (isset($_POST['park'])) {
     $park = $_POST['park'];
-  } 
+  }
   if (isset($_POST['tiere'])) {
     $tiere = $_POST['tiere'];
-  } 
+  }
 
 
-// Store reservation data in the session
-if (!empty($_POST['anreise'] && $_POST['abreise'])) {
-  if (!($anreise <= $abreise)) 
-      {$isOk = 0 ; $error_msg= "Das Anreisedatum sollte vor dem Abreisedatum liegen!"; 
-    }
-  else{
-    $reservation = [
+  // Store reservation data in the session
+  if (!empty($_POST['anreise'] && $_POST['abreise'])) {
+    if (!($anreise <= $abreise)) {
+      $isOk = 0;
+      $error_msg = "Das Anreisedatum sollte vor dem Abreisedatum liegen!";
+    } else {
+      $reservation = [
         'anreise' => $anreise,
         'abreise' => $abreise,
         'room' => $room,
-        'breakfast'=> $breakfast,
-        'park'=> $park,
-        'tiere'=> $tiere
-    ];
-    
-  
-    $string_data = serialize($reservation);
-    file_put_contents("reservations.txt", $string_data, FILE_APPEND);
+        'breakfast' => $breakfast,
+        'park' => $park,
+        'tiere' => $tiere
+      ];
 
-    $conf_msg = "Reservation erfolgreich! Sie können Ihre Reservierungen <a href='./meine_reservations.php'>hier</a> sehen.";
+
+      $string_data = serialize($reservation);
+      file_put_contents("reservations.txt", $string_data, FILE_APPEND);
+
+      $conf_msg = "Reservation erfolgreich! Sie können Ihre Reservierungen <a href='./meine_reservations.php'>hier</a> sehen.";
+    }
+  } else {
+    $isOk = 0;
   }
-}
-  else { $isOk = 0; }
-
 }
 // Optionally, you can redirect the user to a confirmation page
 // header("Location: confirmation.php");
@@ -59,76 +64,81 @@ if (!empty($_POST['anreise'] && $_POST['abreise'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style.css">
-    <title>Reservierungen</title>
-</head>
-<body>
-<?php include "./navbar.php"; ?>
-<div class="bg-image" style="background-image: url('https://images.unsplash.com/photo-1503017964658-e2ff5a583c8e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-position:center; background-size:cover; background-repeat:no-repeat; height:100%;">
 
-<div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100" >
-      <div class="col col-xl-10">
-        <div class="" style="border-radius: 1rem;">
-          <div class="row g-0" >
-            <div class="col-md-6 col-lg-5 d-none d-md-block" >
-              <img src="./room_imgs/reg_process.jpg"
-                alt="res" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-            </div>
-            <div class="col-md-6 col-lg-7 d-flex align-items-center">
-              <div class="body p-4 p-lg-5 text-black">
-                <h1 class="h3 mb-3 font-weight-normal text-center">Anmeldeformular</h1>
-                <?php if($isOk == 0) {echo "<div class='error_msg display-8'> $error_msg </div>";} else { ?> 
-                <?php } echo "<div class='display-8'> $conf_msg </div>" ?> 
-                <form action="" method="post">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link rel="stylesheet" href="./style.css">
+  <title>Reservierungen</title>
+</head>
+
+<body>
+  <?php include "./navbar.php"; ?>
+  <div class="bg-image" style="background-image: url('https://images.unsplash.com/photo-1503017964658-e2ff5a583c8e?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); background-position:center; background-size:cover; background-repeat:no-repeat; height:100%;">
+
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-xl-10">
+          <div class="" style="border-radius: 1rem;">
+            <div class="row g-0">
+              <div class="col-md-6 col-lg-5 d-none d-md-block">
+                <img src="./room_imgs/reg_process.jpg" alt="res" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+              </div>
+              <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                <div class="body p-4 p-lg-5 text-black">
+                  <h1 class="h3 mb-3 font-weight-normal text-center">Anmeldeformular</h1>
+                  <?php if ($isOk == 0) {
+                    echo "<div class='error_msg display-8'> $error_msg </div>";
+                  } else { ?>
+                  <?php }
+                  echo "<div class='display-8'> $conf_msg </div>" ?>
+                  <form action="" method="post">
                     <div class="form-group mt-4">
-                        <label for="anreise">Anreisedatum</label>
-                        <input type="date" name ="anreise" class="form-control input-with-post-icon datepicker" inline="true" placeholder="Anreisedatum">
+                      <label for="anreise">Anreisedatum</label>
+                      <input type="date" name="anreise" class="form-control input-with-post-icon datepicker" inline="true" placeholder="Anreisedatum">
                     </div>
                     <div class="form-group mt-4">
-                        <label for="abreise">Abreisedatum</label>
-                        <input type="date" name ="abreise" class="form-control input-with-post-icon datepicker" inline="true" placeholder="Abreisedatum">
+                      <label for="abreise">Abreisedatum</label>
+                      <input type="date" name="abreise" class="form-control input-with-post-icon datepicker" inline="true" placeholder="Abreisedatum">
                     </div>
                     <select class="form-select mt-4" aria-label="room" name="room">
-                        <option selected disabled value="">Bitte wählen Sie den Zimmertyp...</option>
-                        <option value="Single">Single Zimmer</option>
-                        <option value="Double">Double Zimmer</option>
-                        <option value="Suite">Suite</option>
+                      <option selected disabled value="">Bitte wählen Sie den Zimmertyp...</option>
+                      <option value="Single">Single Zimmer</option>
+                      <option value="Double">Double Zimmer</option>
+                      <option value="Suite">Suite</option>
                     </select>
                     <select class="form-select mt-4" aria-label="breakfast" name="breakfast">
-                        <option selected disabled value="">Möchten Sie Frühstück?</option>
-                        <option value="Ja">Ja</option>
-                        <option value="Nein">Nein</option>
+                      <option selected disabled value="">Möchten Sie Frühstück?</option>
+                      <option value="Ja">Ja</option>
+                      <option value="Nein">Nein</option>
                     </select>
                     <select class="form-select mt-4" aria-label="park" name="park">
-                        <option selected disabled value="">Möchten Sie einen Parkplatz reservieren?</option>
-                        <option value="Ja">Ja</option>
-                        <option value="Nein">Nein</option>
+                      <option selected disabled value="">Möchten Sie einen Parkplatz reservieren?</option>
+                      <option value="Ja">Ja</option>
+                      <option value="Nein">Nein</option>
                     </select>
                     <select class="form-select mt-4" aria-label="tiere" name="tiere">
-                        <option selected disabled value="">Bringen Sie Ihre Haustiere mit?</option>
-                        <option value="Ja">Ja</option>
-                        <option value="Nein">Nein</option>
+                      <option selected disabled value="">Bringen Sie Ihre Haustiere mit?</option>
+                      <option value="Ja">Ja</option>
+                      <option value="Nein">Nein</option>
                     </select>
                     <br>
                     <div class="btns">
-                        <button type="submit" name="reservieren" value="reserieren" class="btn btn-dark">Reservieren</button>
+                      <button type="submit" name="reservieren" value="reserieren" class="btn btn-dark">Reservieren</button>
                     </div>
-                </form>
+                  </form>
                 </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 
-<?php include './footer.php' ?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <?php include './footer.php' ?>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
+
 </html>
