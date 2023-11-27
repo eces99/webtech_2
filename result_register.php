d3f50adfd6ea3b46422f04cf98df731b101f64c9
ece sen <eecesenn@gmail.com>
2023-11-27 15:31:38 +0100

    some updates -styling

--- result_register.php
+++ result_register.php
@@ -1,6 +1,6 @@
 <?php
     
-    if(isset($_POST["register"])){
+    if(isset($_POST["register"]) && isset($_POST["invalidCheck"]) && isset($_POST["anrede"]) && (!empty(($_POST["vorname"]) && ($_POST["lastname"]) ($_POST["username"]) && ($_POST["email"]) && ($_POST["password"]) && ($_POST["password_2"])))){
    
     }
     ?>
@@ -13,12 +13,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
+    <link rel="icon" href="./Images/Untitled-design.svg">
     <title>Registeration</title>
 </head>
 <body>
     <!--Navbar-->
     <?php include_once './navbar.php' ?>
-    <div class="bg-image" style="background-image: url('https://images.unsplash.com/photo-1563209750-fb9498c83efd?q=80&w=1489&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); height:100%; background-repeat: no-repeat; background-size: cover;">
+    <div class="bg-image" style="background-image: url('https://images.unsplash.com/photo-1563209750-fb9498c83efd?q=80&w=1489&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); height:100vh; background-repeat: no-repeat; background-size: cover;">
         <div class="container">   
             <div class="row">
                 <div class="col-6">
