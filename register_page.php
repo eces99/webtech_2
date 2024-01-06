2d4327d131152c90a92a7cd8036ab994f1ccebaf
kenno10101 <kennsanga@gmail.com>
2024-01-06 10:43:28 +0100

    copied code from formhandler->validation error msg

--- register_page.php
+++ register_page.php
@@ -13,25 +13,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (!isset($_POST["anrede"])) {
         $msg_anrede = "Anrede ist benötigt!";
+    } else {
+        $anrede = test_input($_POST["anrede"]);
     }
     if (empty($_POST["vorname"])) {
         $msg_vorname = "Vorname ist benötigt!";
+    } else {
+        $vorname = test_input($_POST["vorname"]);
     }
     if (empty($_POST["lastname"])) {
         $msg_lastname = "Nachname ist benötigt!";
+    } else {
+        $lastname = test_input($_POST["lastname"]);
     }
     if (empty($_POST["username"])) {
         $msg_username = "Benutzername ist benötigt!";
+    } else {
+        $username = test_input($_POST["username"]);
     }
     if (empty($_POST["email"])) {
         $msg_email = "Email ist benötigt!";
     } elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
         $msg_email = "Ungültige E-Mail-Adresse!";
+    } else {
+        $email = test_input($_POST["email"]);
     }
     if (empty($_POST["password"])) {
         $msg_password = "Passwort ist benötigt";
+    } else {
+        $password = test_input($_POST["password"]);
     }
-
     if (empty($_POST["password_2"])) {
         $msg_password_2 = "Passwort nochmal angeben!";
     } elseif ($_POST["password"] !== $_POST["password_2"]) {
@@ -40,10 +51,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!isset($_POST["invalidCheck"])) {
         $msg_checkbox = "Sie müssen den Allgemeinen Geschäftsbedingungen zustimmen.";
     }
-    
+
+    $role = 'anonym';
+    $profile_photo = ''; // You should replace this with the actual blob data when handling file uploads.
+
+    $hashpassword = hash('sha512', $password); // hashed password with hash512 algorithm
+
     // Weiterleitung zur Ergebnisseite, wenn alle Validierungen erfolgreich sind
-    if( isset($_POST["invalidCheck"]) && isset($_POST["anrede"]) && (!empty(($_POST["vorname"]) && ($_POST["lastname"]) && ($_POST["username"]) && ($_POST["email"]) && ($_POST["password"]) && ($_POST["password_2"])))){
-        header("Location:./result_register.php");
+    if (isset($_POST["anrede"]) && isset($_POST["vorname"]) && isset($_POST["lastname"]) && isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["password_2"]) && isset($_POST["invalidCheck"])) {
+
+        require_once "./includes/dbaccess.php";
+
+        // Create a query and insert into using SQL statement
+        $query = "INSERT INTO `users`(`user_id`, `role`, `anrede`, `vorname`, `lastname`, `email`, `username`, `password`, `profile_photo`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"; //placeholders
+
+        // Check query for errors (f.e. duplicate entries for unique usernames/emails)
+        $stmt = $db_obj->stmt_init();
+        if (!$stmt->prepare($query)) {
+            die("SQL error: " . $db_obj->error);
+        }
+
+        $stmt = $db_obj->prepare($query);
+        $stmt->bind_param("isssssssb", $user_id, $role, $anrede, $vorname, $lastname, $email, $username, $hashpassword, $profile_photo);
+
+        if ($stmt->execute()) {
+            // Close the statement and connection when done
+            $stmt->close();
+            $db_obj->close();
+
+            header("Location: ./result_register.php");
+            die();
+        } else {
+            if ($db_obj->errno === 1062) { // error code 1062 for duplicate entries in unique
+                die("Username already exists/Email has already been taken");
+            }
+            die($db_obj->error . " " . $db_obj->errno);
+        }
+    } else {
+        // If trying to access the page without pressing the submit button, send back to the index page
+        //header("Location: ../register_page.php");
     }
 }
 
@@ -85,14 +131,35 @@ function test_input($data)
                 <div class="col-xxs-6 col-md-8 col-lg-6" id="box">
                     <div class="p-3 mb-2 bg-transparent text-dark">
                         <h1 class="h3 mb-3 font-weight-normal text-center">Anmeldeformular</h1>
-                        <form action="./includes/formhandler.inc.php" method="post">
+                        <!--<form action="./includes/formhandler.inc.php" method="post">-->
+                        <form method="post">
                             <select class="form-select" aria-label="anrede" name="anrede">
-                                <option selected disabled value="">Bitte wählen Sie den Anrede</option>
-                                <option value="Herr">Herr</option>
-                                <option value="Frau">Frau</option>
-                                <option value="">Transgender</option>
-                                <option value="">Non-binary/non-conforming</option>
-                                <option value="">Keine Antwort</option>
+                                <option selected disabled value="">Bitte wählen Sie die Anrede</option>
+                                <option value="Herr" <?php if (isset($_POST["anrede"])) {
+                                                            if ($_POST["anrede"] == "Herr") {
+                                                                echo "selected";
+                                                            }
+                                                        } ?>>Herr</option>
+                                <option value="Frau" <?php if (isset($_POST["anrede"])) {
+                                                            if ($_POST["anrede"] == "Frau") {
+                                                                echo "selected";
+                                                            }
+                                                        } ?>>Frau</option>
+                                <option value="Transgender" <?php if (isset($_POST["anrede"])) {
+                                                                if ($_POST["anrede"] == "Transgender") {
+                                                                    echo "selected";
+                                                                }
+                                                            } ?>>Transgender</option>
+                                <option value="Non-binary/non-conforming" <?php if (isset($_POST["anrede"])) {
+                                                                                if ($_POST["anrede"] == "Non-binary/non-conforming") {
+                                                                                    echo "selected";
+                                                                                }
+                                                                            } ?>>Non-binary/non-conforming</option>
+                                <option value="Keine Antwort" <?php if (isset($_POST["anrede"])) {
+                                                                    if ($_POST["anrede"] == "Keine Antwort") {
+                                                                        echo "selected";
+                                                                    }
+                                                                } ?>>Keine Antwort</option>
                             </select>
                             <?php echo "<span class='error_msg'> $msg_anrede </span>" ?>
                             <br>
@@ -121,19 +188,19 @@ function test_input($data)
                             <br>
                             <div class="form-group">
                                 <label for="password">Passwort</label>
-                                <input type="password" class="form-control" name="password" id="password" value="<?php if (isset($_POST["password"])) echo $_POST["password"] ?>">
+                                <input type="password" class="form-control" name="password" id="password" value="">
                             </div>
                             <?php echo "<span class='error_msg'> $msg_password </span>" ?>
                             <br>
                             <div class="form-group">
                                 <label for="password_2">Passwort wiederholen</label>
-                                <input type="password" class="form-control" name="password_2" id="password_2" value="<?php if (isset($_POST["password_2"])) echo $_POST["password_2"] ?>">
+                                <input type="password" class="form-control" name="password_2" id="password_2" value="">
                             </div>
                             <?php echo "<span class='error_msg'> $msg_password_2 </span>" ?>
                             <br>
                             <div class="col-12">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="invalidCheck" name="invalidCheck">
+                                    <input class="form-check-input" type="checkbox" id="invalidCheck" name="invalidCheck" <?php if (isset($_POST["invalidCheck"])) echo "checked" ?>>
                                     <label class="form-check-label" for="invalidCheck">
                                         Ich stimme den Allgemeinen Geschäftsbedingungen zu.
                                     </label><br>
